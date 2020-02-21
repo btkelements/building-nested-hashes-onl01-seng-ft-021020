0a32eea7df5b84725cea57a70a9fedd8a4a8f6bc
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-21 16:38:00 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,11 +19,11 @@
 ##      ]
 ##   }
 ##  }
-
-  #code your solution here:
-
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+##
+##  #code your solution here:
+##
+##  
+##
+##  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
 ##  epic_tragedy
 ## end
